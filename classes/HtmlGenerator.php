4568260b396ea03202b0d9bc087b666494d76625
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-07-04 09:42:48 +0200

    Reste à faire : modifier tout le tableau materialize pour que les index correspondent à leur valeur
    => problème réglé avec une solution temporaire.

--- classes/HtmlGenerator.php
+++ classes/HtmlGenerator.php
@@ -103,6 +103,8 @@ class HtmlGenerator implements Generator
     {
         $this->framework = $framework === Frameworks::FROM_SCRATCH ? false : $framework;
 
+        $this->head($this->meta()->name('viewport')->content('width=device-width, initial-scale=1, shrink-to-fit=no'));
+
         if (isset($this->framework['css'])) {
             foreach ($this->framework['css'] as $css) {
                 $css['integrity'] = isset($css['integrity']) ? $css['integrity'] : '';
